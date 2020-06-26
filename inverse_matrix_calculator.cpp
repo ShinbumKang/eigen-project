52b0aabd4a8ab7a163d4622b30567b4df26516ec
Shinbum <21500005@handong.edu>
2020-06-26 06:42:02 -0700

    upgrade

--- inverse_matrix_calculator.cpp
+++ inverse_matrix_calculator.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <Eigen/LU> 
+
  
 using namespace Eigen;
 using namespace std;
@@ -9,28 +11,46 @@ int main()
   MatrixXd input(2,2);
   MatrixXd ans(2,2);
   float det;
+  int size_want,i,j;
 
-  cout << "Enter matrix(2X2)" << endl;                               
-  cin >> input(0,0) >> input(0,1) >> input(1,0) >> input(1,1);  
-  
-  det = (input(0,0)*input(1,1) - input(0,1)*input(1,0));
-  
-  if(det == 0)
+  cout << "what size matrix do you want?" << endl;
+
+  cin >> size_want; 
+
+  if(size_want <= 0)
   {
-    cout << "Reverse matrix does not exist." << endl;
-    return 0;   
+    cout << "Size must be at least 1" << endl;
+    return 0;
   }
 
-  ans(0,0) = input(1,1);
-  ans(1,0) = (-1)*input(1,0);
-  ans(0,1) = (-1)*input(0,1);
-  ans(1,1) = input(0,0);
- 
-  ans = ans / det; 
+  input.resize(size_want,size_want);
+  ans.resize(size_want,size_want);
+
+  //cout << "Enter matrix" << endl;
+
+  for(i=0;i<size_want;i++)
+  {
+    if(i+1==1)
+      cout << "Enter " << i+1 << "st column" << endl;
+    else if(i+1==2)
+      cout << "Enter " << i+1 << "nd column" << endl;
+    else if(i+1==3)
+      cout << "Enter " << i+1 << "rd column" << endl;
+    else
+      cout << "Enter " << i+1 << "th column" << endl;
+    for(j=0;j<size_want;j++)
+      cin >> input(j,i);  
+  }                              
+  
+  det = input.determinant();
+
+  ans = input.inverse(); 
   
   cout << endl << "Entered matrix is" << endl << input << endl << endl;
 
-  cout << "Inverse matrix is" << endl << ans << endl;
+  cout << "Inverse matrix is" << endl << input.inverse() << endl << endl;
+
+  cout << "Determinant is " << input.determinant() << endl;
 
   return 0;
 }
